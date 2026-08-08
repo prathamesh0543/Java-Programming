5bcc6a3564c82af342c218372c0c648563a082f3
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-08 14:55:39 +0530

    higher method

---
+++ src/collection/aug_8_4_highermethod_.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.TreeSet;
+
+public class aug_8_4_highermethod_ {
+
+	public static void main(String[] args) {
+
+		TreeSet<Integer> p1= new TreeSet<Integer> ();
+
+		 p1.add(12);
+		 p1.add(1);
+		 p1.add(44);
+		 System.out.println(p1);
+		 System.out.println(p1.higher(40));
+
+	}
+
+}
