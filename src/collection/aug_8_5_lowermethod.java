e290001c3f57fb0a8f7a131256d1dfaa402f4d1d
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-08 14:57:00 +0530

    lower method

---
+++ src/collection/aug_8_5_lowermethod.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.TreeSet;
+
+public class aug_8_5_lowermethod {
+
+	public static void main(String[] args) {
+		
+		 TreeSet<Integer> p1= new TreeSet<Integer> ();
+
+		 p1.add(12);
+		 p1.add(1);
+		 p1.add(44);
+		 System.out.println(p1);
+		 System.out.println(p1.lower(10));
+		 System.out.println(p1.lower(1));
+
+
+	}
+
+}
