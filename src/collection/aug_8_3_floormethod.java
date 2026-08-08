3cc9ab943484b7f470f3e394b4331414c90e92d0
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-08 14:52:24 +0530

    floor method in tree set

---
+++ src/collection/aug_8_3_floormethod.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.TreeSet;
+
+public class aug_8_3_floormethod {
+
+	public static void main(String[] args) {
+
+
+		 TreeSet<Integer> p1= new TreeSet<Integer> ();
+
+		 p1.add(12);
+		 p1.add(1);
+		 p1.add(44);
+		 System.out.println(p1);
+		 System.out.println(p1.floor(5));
+		 System.out.println(p1.floor(47));
+	}
+
+}
