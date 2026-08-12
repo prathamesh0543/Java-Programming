25746af9e6fbfec93def77d371f814b8b49e1ee7
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-12 23:25:19 +0530

    Basic fact: 1 to 10 factorial

---
+++ src/home_work_practice/Aug_12_fact1to10.java
@@ -0,0 +1,16 @@
+package home_work_practice;
+
+public class Aug_12_fact1to10 {
+
+	public static void main(String[] args) {
+
+		int fact=1;
+		for(int i=1;i<=10;i++)
+		{
+			fact*=i;
+		}
+		System.out.println("fact of 1 to 10 : "+fact);
+
+	}
+
+}
