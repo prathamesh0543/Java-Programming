3b3af641dede1b6a565d4269340648c91dac1034
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-12 23:16:10 +0530

    Basic sum : sum of 1 to 50

---
+++ src/home_work_practice/Aug_12_sumOf1to50.java
@@ -0,0 +1,17 @@
+package home_work_practice;
+
+public class Aug_12_sumOf1to50 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<=50;i++) {
+			
+			sum=sum+i;
+			
+		}
+		System.out.println("sum of 1 to 50 is : "+sum);
+
+
+	}
+
+}
