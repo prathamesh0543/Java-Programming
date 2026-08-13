237773b76eedc4d23cceadf30b233c513cfbbbab
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-13 22:36:53 +0530

    triangle pattern

---
+++ src/pattern_practice/Aug_13_triangle.java
@@ -0,0 +1,18 @@
+package pattern_practice;
+
+public class Aug_13_triangle {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+
+	}
+
+}
