e7307d745f9eb6019c3b4db2015140960a70a745
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-16 15:40:49 +0530

    factorial

---
+++ src/home_work_practice/aug_16_fact.java
@@ -0,0 +1,18 @@
+package home_work_practice;
+
+public class aug_16_fact {
+
+	public static void main(String[] args) {
+		
+		int num=5;
+		int fact=1;
+		for(int i=1;i<=num;i++)
+		{
+			fact*=i;
+			
+		}
+		System.out.println("The 5 is gactorial is : "+fact);
+
+	}
+
+}
