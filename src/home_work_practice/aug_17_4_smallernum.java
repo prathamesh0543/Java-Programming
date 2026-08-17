d66f0048b602458ff32836a12d2156c69cf94345
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-17 11:33:15 +0530

    ef_else_smallest

---
+++ src/home_work_practice/aug_17_4_smallernum.java
@@ -0,0 +1,17 @@
+package home_work_practice;
+
+public class aug_17_4_smallernum {
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=20;
+		if(num1<num2)
+		{
+			System.out.println("num1 is less than num2");
+		}
+		else
+		{
+			System.out.println("num2 is less than num1");
+		}
+	}
+
+}
