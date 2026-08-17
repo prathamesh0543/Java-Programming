2a51e81955ad49ec1d0095de0a11bac9038b4ab3
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-17 11:32:16 +0530

    nested_if_else

---
+++ src/home_work_practice/aug_17_if_elsepractice.java
@@ -0,0 +1,18 @@
+package home_work_practice;
+
+public class aug_17_if_elsepractice {
+
+	public static void main(String[] args) {
+		
+		int age=20;
+		if(age>-18) {
+			System.out.println("eligible for voting. ");
+			
+		}
+		else {
+			System.out.println("not eligible for voting. ");
+		}
+
+	}
+
+}
