1cc82ed7313259fdc7a194c3291a44f9af47f6a2
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-17 11:32:54 +0530

    if_else

---
+++ src/home_work_practice/aug_17_even_odd.java
@@ -0,0 +1,18 @@
+package home_work_practice;
+
+public class aug_17_even_odd {
+
+	public static void main(String[] args) {
+		int num=10;
+		if(num%2==0)
+		{
+			System.out.println(num+" Num is Even");
+		}
+			else
+		{
+			System.out.println(num+" Num is Odd");
+		}
+
+	}
+
+}
