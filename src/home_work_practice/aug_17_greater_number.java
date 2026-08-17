9b43c2b64d8b40c263cd2d6d6fb9f608b6363757
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-17 11:32:40 +0530

    if_else

---
+++ src/home_work_practice/aug_17_greater_number.java
@@ -0,0 +1,19 @@
+package home_work_practice;
+
+public class aug_17_greater_number {
+
+	public static void main(String[] args) {
+		int num=10,num1=20;
+		if(num>num1)
+		{
+			System.out.println("num is greater than num1");
+		}
+		else
+		{
+			System.out.println("num is less than num1");
+		}
+		
+
+	}
+
+}
