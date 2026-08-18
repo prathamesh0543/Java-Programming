b22fcffd87e6a430f9ea904417842f810bf12bb1
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-18 13:55:18 +0530

    Three numbers Find greater number

---
+++ src/home_work_practice/aug_18_threenumbersGreater.java
@@ -0,0 +1,25 @@
+package home_work_practice;
+
+public class aug_18_threenumbersGreater {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		
+		if(a>b && a>c )
+		{
+			System.out.println("a is a greater");
+		}
+
+		else if(b>c)
+		{
+			System.out.println("b is greater");
+		}
+		else
+		{
+			System.out.println("c is greater");
+		}
+	}
+
+}
