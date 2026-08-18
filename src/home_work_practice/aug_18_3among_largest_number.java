b899ad5a5f2a3302fadd6522302676ccb53adbec
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-18 13:56:30 +0530

    Find second larger number

---
+++ src/home_work_practice/aug_18_3among_largest_number.java
@@ -0,0 +1,33 @@
+package home_work_practice;
+
+public class aug_18_3among_largest_number {
+
+	public static void main(String[] args) {
+		
+		int a=29;
+		int b=90;
+		int c=56;
+		int d=78;
+		
+		if(a>b && a>c && a>d)
+		{
+			System.out.println("a is greater");
+		}
+		else if(b>c && b>d)
+		{
+			System.out.println("b is greater");			
+		}
+		else if(c>d)
+		{
+			System.out.println("c is greater");			
+		}
+		else
+		{
+			System.out.println("d is greater");
+		}
+		
+		
+
+	}
+
+}
