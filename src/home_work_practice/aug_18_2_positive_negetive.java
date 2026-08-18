9e9a899a040052cb8853b0ce14f02eda492da3bf
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-18 13:56:02 +0530

    Find largest number

---
+++ src/home_work_practice/aug_18_2_positive_negetive.java
@@ -0,0 +1,22 @@
+package home_work_practice;
+
+public class aug_18_2_positive_negetive {
+
+	public static void main(String[] args) {
+		
+		int num=-5;
+		if(num>0)
+		{
+			System.out.println("number is positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("number is negetive");
+		}else
+		{
+			System.out.println("none");
+		}
+
+	}
+
+}
