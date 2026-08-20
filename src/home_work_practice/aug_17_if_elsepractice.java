1f6140bfc1568118cc3c85dfefdfdbb5c3a3259c
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-20 16:22:05 +0530

    if else

--- src/home_work_practice/aug_17_if_elsepractice.java
+++ src/home_work_practice/aug_17_if_elsepractice.java
@@ -5,7 +5,7 @@ public class aug_17_if_elsepractice {
 	public static void main(String[] args) {
 		
 		int age=20;
-		if(age>-18) {
+		if(age>=18) {
 			System.out.println("eligible for voting. ");
 			
 		}
