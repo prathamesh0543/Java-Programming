fa38292b3f111ae6a9ca965b9a4d02efd524af39
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-21 14:28:32 +0530

    check sum of 1 to 50

---
+++ src/home_work_practice/aug_20_1_to_50_sum.java
@@ -0,0 +1,17 @@
+package home_work_practice;
+
+public class aug_20_1_to_50_sum {
+
+	public static void main(String[] args) {
+		int sum=0;
+
+		for(int i=1;i<=50;i++)
+		{
+			sum=sum+i;
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
