fa94a1504daec0718d033f5b57b9a7bdc7b1c3da
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-21 14:14:52 +0530

    given number check the count

---
+++ src/home_work_practice/aug_20_count_check.java
@@ -0,0 +1,21 @@
+package home_work_practice;
+
+public class aug_20_count_check {
+
+	public static void main(String[] args) {
+		
+		
+		int num=12345;
+		int c=0;
+		
+		while(num>0)
+		{
+			num=num/10;
+			c++;
+
+		}
+		System.out.println(c);
+		
+	}
+
+}
