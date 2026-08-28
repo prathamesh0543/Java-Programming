c7d0ff9cbb05a79dc6fae0c0f42f04e3c3482339
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-29 01:12:52 +0530

    Prime no

---
+++ src/home_work_practice/aug_29_1to100prime_no.java
@@ -0,0 +1,25 @@
+package home_work_practice;
+
+public class aug_29_1to100prime_no {
+
+    public static void main(String[] args) {
+
+        for(int i=2; i<=100; i++)
+        {
+            int c=0;
+
+            for(int j=1; j<=i; j++)
+            {
+                if(i%j==0)
+                {
+                    c++;
+                }
+            }
+
+            if(c==2)
+            {
+                System.out.println(c+""+i);
+            }
+        }
+    }
+}
