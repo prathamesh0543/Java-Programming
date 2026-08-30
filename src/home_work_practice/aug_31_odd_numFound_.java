c42708d731fb2b43fcf9e07ca08e30f736870ae3
prathamesh0543 <prathamesh0543@gmail.com>
2026-08-31 00:14:51 +0530

    Odd number find using while loop

---
+++ src/home_work_practice/aug_31_odd_numFound_.java
@@ -0,0 +1,23 @@
+package home_work_practice;
+
+public class aug_31_odd_numFound_ {
+
+	public static void main(String[] args) {
+		int num=58321;
+		int c=0;
+		
+		while(num>0)
+		{
+			int digit=num%10;
+			if(digit%2!=0)
+			{
+				c++;
+//				System.out.println(digit);
+			}
+			num=num/10;
+		}
+		System.out.println(c);
+
+	}
+
+}
