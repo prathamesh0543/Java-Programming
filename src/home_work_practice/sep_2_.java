d033b05c5e18d6de3add1cb6329fa972da6a5504
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-02 20:35:02 +0530

    Given num create reverse using whileloop

---
+++ src/home_work_practice/sep_2_.java
@@ -0,0 +1,19 @@
+package home_work_practice;
+
+public class sep_2_ {
+	
+	public static void main(String[] args) {
+		
+		int num=58321;
+		int rev=0;
+		
+		while(num>0)
+		{
+			int digit=num%10;
+			rev=rev*10+digit;
+			num=num/10;
+		}
+		System.out.println(rev);
+	
+}
+}
