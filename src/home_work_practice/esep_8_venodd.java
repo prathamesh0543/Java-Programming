30fccc97ea474b68b3ab33341943f09a50936f8f
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-08 23:34:27 +0530

    Even odd number find

---
+++ src/home_work_practice/esep_8_venodd.java
@@ -0,0 +1,20 @@
+package home_work_practice;
+
+public class esep_8_venodd {
+
+	
+		public static void main(String[] args) {
+			int num=10;
+			if(num%2==0)
+			{
+				System.out.println(num+" Num is Even");
+			}
+				else
+			{
+				System.out.println(num+" Num is Odd");
+			}
+
+
+	}
+
+}
