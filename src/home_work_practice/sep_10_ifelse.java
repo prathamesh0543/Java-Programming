1785e6c9f733af51afccadc016e223a6168391a4
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-10 18:38:50 +0530

    if else

---
+++ src/home_work_practice/sep_10_ifelse.java
@@ -0,0 +1,27 @@
+package home_work_practice;
+
+public class sep_10_ifelse {
+
+	public static void main(String[] args) {
+		
+		int marks=70;
+		if(marks>=80) {
+			System.out.println("Topper");
+		}
+		else if(marks>=78){
+			System.out.println("second topper");
+		}
+		else if(marks>=54)
+		{
+			System.out.println("third");
+			
+		}
+		else if(marks>=35){
+			System.out.println("pass");
+		}else {
+			System.out.println("fail");
+		}
+
+	}
+
+}
