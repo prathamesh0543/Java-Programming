5855272071af7a951787c3a041b3dd98e14b48d1
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-14 23:09:01 +0530

    Even number sum

---
+++ src/home_work_practice/sep_15_.java
@@ -0,0 +1,18 @@
+package home_work_practice;
+
+public class sep_15_ {
+
+	public static void main(String[] args) {
+		int sum=0;
+
+		for(int i=1;i<=10;i++) {
+			if(i%2==0)
+			{
+				sum+=i;
+			}
+		}
+		System.out.println("Even number : "+sum);
+
+	}
+
+}
