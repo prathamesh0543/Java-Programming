3ca7e3de90a0013aae51369c1bc89f3f62bfc0d7
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-15 23:50:11 +0530

    Array Dequeue

---
+++ src/home_work_practice/sep_15_deque.java
@@ -0,0 +1,19 @@
+package home_work_practice;
+
+import java.util.ArrayDeque;
+
+public class sep_15_deque {
+
+	public static void main(String[] args) {
+		
+		ArrayDeque p=new ArrayDeque();
+		p.add(4);
+		p.add(3);
+		p.add("abs");
+		p.add(12.4);
+;
+		System.out.println(p);
+
+	}
+
+}
