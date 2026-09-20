3ec2c7786c841068d102521b744b22321a5d2738
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-20 22:55:48 +0530

    Dequeue collection method

---
+++ src/home_work_practice/sep_20_.java
@@ -0,0 +1,36 @@
+package home_work_practice;
+
+import java.util.ArrayDeque;
+
+public class sep_20_ {
+
+	public static void main(String[] args) {
+		
+		ArrayDeque p=new ArrayDeque();
+		p.add(4);
+		p.add(3);
+		p.add("abs");
+		p.add(12.4);
+;
+		System.out.println(p);
+		
+//		p.addFirst(100);
+//		p.addLast(2000);
+//		p.offerFirst(200);
+//		p.offerLast(900);
+//		System.out.println(p);
+		
+//		System.out.println(p.peekFirst()+""+p.peekLast());
+//		System.out.println(p.pollFirst()+""+p.pollLast());
+//		System.out.println(p);
+		
+		System.out.println();
+		System.out.println(p.getFirst());
+		System.out.println(p.getLast());
+		
+
+	
+
+	}
+
+}
