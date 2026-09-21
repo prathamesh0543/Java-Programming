f532e67d3ca6566ac58afe8643dd0c78ba3e7be1
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-22 00:16:42 +0530

    collection

---
+++ src/home_work_practice/sep_22.java
@@ -0,0 +1,16 @@
+package home_work_practice;
+
+import java.util.function.Predicate;
+
+public class sep_22 {
+
+	public static void main(String[] args) {
+
+
+Predicate<Integer>even=n->n%2==0;
+		
+		System.out.println(even.test(55));
+
+	}
+
+}
