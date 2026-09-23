6bc879fd462c3e35d6d40b23d320290291b9019e
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-23 23:48:15 +0530

    Sep

--- src/ArrayCodes_JM/found_.java
+++ src/ArrayCodes_JM/found_.java
@@ -3,7 +3,6 @@ package ArrayCodes_JM;
 public class found_ {
 	public static void main(String[] args) {
 		
-		        // TODO Auto-generated method stub
 		        int a[] = {1,2,3,4,5,6,7,8,9,10};
 
 		        for (int i = 0; i < a.length; i++) {
