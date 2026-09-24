0f58b7b0f102d30ce81be7e6d8456e942f04a247
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-25 00:13:43 +0530

    Multithreading

---
+++ src/home_work_practice/sep_25.java
@@ -0,0 +1,23 @@
+package home_work_practice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class sep_25 {
+
+	
+		
+		public static void main(String[] args) {
+			Map<Integer,String> map=new HashMap<>();
+			map.put(101, "pc");
+			map.put(102, "rc");
+			map.put(103, "tc");
+			 
+			ArrayList<Integer> a =new ArrayList<>(map.keySet());
+		ArrayList<String> b=new ArrayList<>(map.values());
+		System.out.println(a + " "+b);
+
+	}
+
+}
