f40860e72013d1e63cae717b65fb4b6ee01ce875
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-25 00:01:04 +0530

    string

---
+++ src/home_work_practice/sep.java
@@ -0,0 +1,30 @@
+package home_work_practice;
+
+public class sep {
+	public static void main(String[] args) {
+
+		String arr[]= {"prathamesh","chavan"};
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			String nm[]=arr[i].split(" ");
+			
+			StringBuilder sb=new StringBuilder();
+			
+			for(String ch:nm)
+			{
+				sb.append(Character.toUpperCase(ch.charAt(0))).append(ch.substring(1)).append(" ");
+				
+				
+			}
+			arr[i]=sb.toString();
+			
+		}
+		for( String name:arr)
+		{
+			System.out.print(name);
+		}
+
+	}
+
+}
