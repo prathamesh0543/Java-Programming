590791e62a9061d3f5a1ec71eef53b5b8a4410b1
prathamesh0543 <prathamesh0543@gmail.com>
2026-09-26 23:08:35 +0530

    Digit of sum single and double

---
+++ src/home_work_practice/sep_26.java
@@ -0,0 +1,28 @@
+package home_work_practice;
+
+public class sep_26 {
+
+	public static void main(String[] args) {
+
+			// TODO Auto-generated method stub
+			
+
+			        int arr[] = {10, 2, 13, 5, 22};
+			        int singleDigitSum = 0;
+			        int doubleDigitSum = 0;
+
+			        for (int i = 0; i < arr.length; i++) {
+
+			            if (arr[i] <= 9) {
+			                singleDigitSum = singleDigitSum + arr[i];
+			            } 
+			            else if (arr[i] >= 10 && arr[i] <= 99) {
+			                doubleDigitSum = doubleDigitSum + arr[i];
+			            }
+			        }
+
+			        System.out.println("Single digit sum: " + singleDigitSum);
+			        System.out.println("Double digit sum: " + doubleDigitSum);
+	}
+
+}
